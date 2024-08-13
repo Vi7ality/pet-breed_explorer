ddc1e9079e46e50d2268bcbe9d8fba6c52abde11
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-13 12:11:10 +0300

    collection added to the component

--- src/app/components/pets-list.tsx
+++ src/app/components/pets-list.tsx
@@ -1,14 +1,23 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import getPets from '../../../lib/utils/getPets';
 
 export default function PetsList() {
   const [petList, setPetList] = useState([]);
+  console.log(petList);
+
+  const getPetList = useCallback(async () => {
+    try {
+      const pets = await getPets();
+      pets && setPetList(pets);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   useEffect(() => {
-    getPets()
-      .then((pets) => setPetList(pets))
-      .finally(console.log('petList', petList));
-  });
+    getPetList();
+  }, [getPetList]);
   return <ul></ul>;
 }
