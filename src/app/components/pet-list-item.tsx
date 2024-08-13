fb054c70d878bc80211300f866d5901cea97871e
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-13 14:28:57 +0300

    ***

--- src/app/components/pet-list-item.tsx
+++ src/app/components/pet-list-item.tsx
@@ -6,9 +6,14 @@ interface ListItemProps {
 
 export default function PetListItem({ id, url, name }: ListItemProps) {
   return (
-    <li key={id} className="overflow-hidden rounded-lg">
-      <img src={url} alt={name} className="w-full h-full object-cover"></img>
-      <h3>{name}</h3>
+    <li
+      key={id}
+      className="hover:translateY(0) relative overflow-hidden rounded-lg"
+    >
+      <img src={url} alt={name} className="h-full w-full object-cover"></img>
+      <h3 className="absolute bottom-0 left-0 w-full bg-green-600 py-5 text-center text-white">
+        {name}
+      </h3>
     </li>
   );
 }
