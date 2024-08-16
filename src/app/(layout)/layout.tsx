31029bdb04cd65edb89d7d851ba99244d41d495b
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-16 18:04:10 +0300

    props added to children

--- src/app/(layout)/layout.tsx
+++ src/app/(layout)/layout.tsx
@@ -1,7 +1,11 @@
 import Footer from '../components/footer';
 import Header from '../components/header';
 
-export default function Layout({ children }) {
+interface Props {
+  children?: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
