048ea509f581ab24b0f7e69149133e7505b70121
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-16 23:45:39 +0300

    pet list typized

--- src/app/components/pet-list.tsx
+++ src/app/components/pet-list.tsx
@@ -4,13 +4,22 @@ import { useCallback, useEffect, useState } from 'react';
 import getPets from '../../../lib/utils/getPets';
 import PetListItem from './pet-list-item';
 
+interface Pet {
+  id: string;
+  name: string;
+  image: {
+    url: string;
+  };
+  species: 'cat' | 'dog';
+  [key: string]: unknown;
+}
+
 export default function PetList() {
-  const [petList, setPetList] = useState([]);
-  console.log(petList);
+  const [petList, setPetList] = useState<Pet[]>([]);
 
   const getPetList = useCallback(async () => {
     try {
-      const pets = await getPets();
+      const pets: Pet[] = await getPets();
       pets && setPetList(pets);
     } catch (error) {
       console.error(error);
@@ -21,7 +30,7 @@ export default function PetList() {
     getPetList();
   }, [getPetList]);
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+    <ul className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {petList.length > 0 &&
         petList.map((item) => (
           <PetListItem
