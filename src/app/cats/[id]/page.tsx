fcc54905765d62d088daad0e0b1ae31d5bc1cbe0
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-16 17:49:58 +0300

    cat breed page updated

--- src/app/cats/[id]/page.tsx
+++ src/app/cats/[id]/page.tsx
@@ -9,8 +9,19 @@ export interface PageProps {
   params: { id: string };
 }
 
+interface BreedInfo {
+  reference_image_id: string;
+  name: string;
+  description: string;
+  origin: string;
+  temperament: string;
+  hypoallergenic?: number;
+  intelligence?: number;
+  [key: string]: unknown;
+}
+
 export default function Page({ params }: PageProps) {
-  const [breedInfo, setBreedInfo] = useState([]);
+  const [breedInfo, setBreedInfo] = useState<BreedInfo | null>(null);
 
   const getBreedInfo = useCallback(async () => {
     const data = await getCatBreed(params.id);
@@ -29,145 +40,38 @@ export default function Page({ params }: PageProps) {
     <main>
       <Header />
       <Container>
-        <div className="flex flex-col overflow-hidden rounded-lg bg-white shadow-lg md:flex-row">
-          {/* Image Section */}
-          <div className="md:w-1/2">
-            <Image
-              width={600}
-              height={400}
-              alt={`${breedInfo.name} image`}
-              src={`https://cdn2.thecatapi.com/images/${breedInfo.reference_image_id}.jpg`}
-              className="h-full w-full object-cover"
-            />
-          </div>
-
-          {/* Text Section */}
-          <div className="flex flex-col justify-center p-6 md:w-1/2">
-            <h1 className="mb-4 text-3xl font-bold text-gray-900">
-              {breedInfo.name}
-            </h1>
-            <p className="mb-4 text-gray-700">{breedInfo.description}</p>
-            <p className="mb-2 text-gray-600">
-              <strong>Origin:</strong> {breedInfo.origin}
-            </p>
-            <p className="text-gray-600">
-              <strong>Temperament:</strong> {breedInfo.temperament}
-            </p>
-          </div>
-        </div>
+        {breedInfo && (
+          <section className="flex flex-col overflow-hidden bg-white shadow-lg md:flex-row">
+            <div className="md:w-1/2">
+              <Image
+                width={600}
+                height={400}
+                alt={`${breedInfo.name} image`}
+                src={`https://cdn2.thecatapi.com/images/${breedInfo.reference_image_id}.jpg`}
+                className="h-full w-full object-cover"
+              />
+            </div>
+            <div className="flex flex-col justify-center p-6 md:w-1/2">
+              <h1 className="mb-4 text-3xl font-bold text-gray-900">
+                {breedInfo.name}
+              </h1>
+              <p className="mb-4 text-gray-700">{breedInfo.description}</p>
+              <p className="mb-2 text-gray-600">
+                <strong>Origin:</strong> {breedInfo.origin}
+              </p>
+              <p className="text-gray-600">
+                <strong>Temperament:</strong> {breedInfo.temperament}
+              </p>
+              <p className="text-gray-600">
+                <strong>hypoallergenic:</strong> {breedInfo.hypoallergenic}
+              </p>
+              <p className="text-gray-600">
+                <strong>intelligence:</strong> {breedInfo.intelligence}
+              </p>
+            </div>
+          </section>
+        )}
       </Container>
     </main>
   );
 }
-
-// adaptability
-// :
-// 5
-// affection_level
-// :
-// 5
-// alt_names
-// :
-// ""
-// cfa_url
-// :
-// "http://cfa.org/Breeds/BreedsAB/AmericanBobtail.aspx"
-// child_friendly
-// :
-// 4
-// country_code
-// :
-// "US"
-// country_codes
-// :
-// "US"
-// description
-// :
-// "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion."
-// dog_friendly
-// :
-// 5
-// energy_level
-// :
-// 3
-// experimental
-// :
-// 0
-// grooming
-// :
-// 1
-// hairless
-// :
-// 0
-// health_issues
-// :
-// 1
-// hypoallergenic
-// :
-// 0
-// id
-// :
-// "abob"
-// indoor
-// :
-// 0
-// intelligence
-// :
-// 5
-// lap
-// :
-// 1
-// life_span
-// :
-// "11 - 15"
-// name
-// :
-// "American Bobtail"
-// natural
-// :
-// 0
-// origin
-// :
-// "United States"
-// rare
-// :
-// 0
-// reference_image_id
-// :
-// "hBXicehMA"
-// rex
-// :
-// 0
-// shedding_level
-// :
-// 3
-// short_legs
-// :
-// 0
-// social_needs
-// :
-// 3
-// stranger_friendly
-// :
-// 3
-// suppressed_tail
-// :
-// 1
-// temperament
-// :
-// "Intelligent, Interactive, Lively, Playful, Sensitive"
-// vcahospitals_url
-// :
-// "https://vcahospitals.com/know-your-pet/cat-breeds/american-bobtail"
-// vetstreet_url
-// :
-// "http://www.vetstreet.com/cats/american-bobtail"
-// vocalisation
-// :
-// 3
-// weight
-// :
-// {imperial: '7 - 16', metric: '3 - 7'}
-// wikipedia_url
-// :
-// "https://en.wikipedia.org/wiki/American_Bobtail"
