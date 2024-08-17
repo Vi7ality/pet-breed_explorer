1140496eb915f75fdc3c7d091c1155ee1e7a61ff
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-08-17 12:13:04 +0300

    ***

--- src/app/(layout)/about/page.tsx
+++ src/app/(layout)/about/page.tsx
@@ -1,10 +1,9 @@
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Footer from '../../components/footer';
+import Header from '../../components/header';
 
 export default function About() {
   return (
     <>
-      <Header />
       <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-6 text-gray-800">
         <section className="w-full max-w-4xl">
           <h1 className="mb-8 text-center text-4xl font-bold">About Us</h1>
@@ -31,7 +30,6 @@ export default function About() {
           </p>
         </section>
       </main>
-      <Footer />
     </>
   );
 }
